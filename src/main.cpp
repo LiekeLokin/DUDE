a75b078818d213ea7ef330dc13ba83290495bf2c
Lieke <68234092+LiekeLokin@users.noreply.github.com>
2022-10-18 17:19:02 +0200

    Add abort to fatal warning (H out of bounds)

--- src/main.cpp
+++ src/main.cpp
@@ -381,6 +381,7 @@ for (int p=1;p<=1;p++){				//superloop!!!!!!!!!!!!
 		doCheckQsp(bedflow, H2O, sand, q_in, cfg);
 		if ((H >= 100*q_in) || (H <= 0.01*q_in)) {
 			DUDE_LOG(fatal) << "Water depth out of bounds, H: " << H;
+			abort();
 		}
 		vec u0_b(cfg.Npx);
 		vec U0_mean(cfg.Npx);
